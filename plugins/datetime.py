dd44126f3f88eb46ffb9612f63ac804e89f80154
jammus <jammus@gmail.com>
2024-09-21 15:37:21 +0800

    Blasts from the past

--- plugins/datetime.py
+++ plugins/datetime.py
@@ -1,3 +1,4 @@
+from math import floor 
 from datasette import hookimpl
 from datetime import datetime
 
@@ -14,9 +15,14 @@ def end_of_year(year):
     return datetime(year + 1, 1, 1).timestamp()
 
 
+def years(seconds: int):
+    return floor(seconds / 60 / 60 / 24 / 30 / 12)
+
+
 @hookimpl
 def extra_template_vars():
     return {
+        "years": years,
         "current_date": current_date,
         "start_of_year": start_of_year,
         "end_of_year": end_of_year,
