e535219848ecc310f2ab6b82b448735d6cc0bbab
jammus <jammus@gmail.com>
2024-09-22 14:31:04 +0800

    Move queries out of templates

---
+++ plugins/report_queries.py
@@ -0,0 +1,142 @@
+from datasette import hookimpl
+from datasette.app import Datasette
+
+
+def fetch_top_artists(datasette: Datasette):
+    async def fetch(start_timestamp: int, end_timestamp:int):
+        db = datasette.get_database()
+        query = """
+            select
+              artist, count(1) as listens, discovered,
+              (discovered >= cast(:start as integer)) as new
+            from
+              playlist as p
+            join
+              v_artist_details as v on p.artist = v.name
+            where
+              uts_timestamp >= :start and
+              uts_timestamp < :end 
+            group by
+              p.artist
+            order by
+              listens desc
+            limit 20
+        """
+        return (await db.execute(
+            query,
+            {"start": start_timestamp, "end": end_timestamp})
+        ).rows
+    return fetch
+
+
+def fetch_top_albums(datasette: Datasette):
+    async def fetch(start_timestamp: int, end_timestamp:int):
+        db = datasette.get_database()
+        query = """
+            select
+              p.artist, name, count(1) as listens, discovered,
+              (discovered >= cast(:start as integer)) as new
+            from
+              playlist as p
+            join
+              v_album_details as v on p.album = v.name
+              and p.artist = v.artist
+            where
+              uts_timestamp >= :start and
+              uts_timestamp < :end 
+            group by
+              p.artist, p.album
+            order by
+              listens desc
+            limit 20
+        """
+        return (await db.execute(
+            query,
+            {"start": start_timestamp, "end": end_timestamp})
+        ).rows
+    return fetch
+
+
+def fetch_top_tracks(datasette: Datasette):
+    async def fetch(start_timestamp: int, end_timestamp:int):
+        db = datasette.get_database()
+        query = """
+            select
+              p.artist, name, count(1) as listens, discovered,
+              (discovered >= cast(:start as integer)) as new
+            from
+              playlist as p
+            join
+              v_track_details as v on p.song = v.name
+                                      and p.artist = v.artist
+            where
+              uts_timestamp >= :start and
+              uts_timestamp < :end 
+            group by
+              p.artist, p.song
+            order by
+              listens desc
+            limit 20
+        """
+        return (await db.execute(
+            query,
+            {"start": start_timestamp, "end": end_timestamp})
+        ).rows
+    return fetch
+
+
+def fetch_blast_artists(datasette: Datasette):
+    async def fetch(start_timestamp: int, end_timestamp:int):
+        db = datasette.get_database()
+        query = """
+          select
+            *, (first_listen_this_period - previous_listen) as since
+          from (
+            select
+              p1.artist, count(1) as past_listens,
+              max(p1.uts_timestamp) as previous_listen, current_listens,
+              first_listen_this_period
+            from
+              playlist as p1
+            join (
+                select
+                  artist, count(1) as current_listens,
+                  min(p2.uts_timestamp) as first_listen_this_period
+                from
+                  playlist as p2
+                where
+                  uts_timestamp >= :start and
+                  uts_timestamp < :end 
+                group by
+                  artist
+              ) as this_p
+              on p1.artist = this_p.artist
+            where
+              p1.uts_timestamp < :start
+            and
+              current_listens > 5
+            group by
+            p1.artist
+          )
+          where
+            since >= (365 * 24 * 60 * 60)
+            and past_listens > 5
+          order by
+            since desc
+          limit 20
+        """
+        return (await db.execute(
+            query,
+            {"start": start_timestamp, "end": end_timestamp})
+        ).rows
+    return fetch
+
+
+@hookimpl
+def extra_template_vars(datasette):
+    return {
+        "fetch_top_artists": fetch_top_artists(datasette),
+        "fetch_top_albums": fetch_top_albums(datasette),
+        "fetch_top_tracks": fetch_top_tracks(datasette),
+        "fetch_blast_artists": fetch_blast_artists(datasette),
+    }
